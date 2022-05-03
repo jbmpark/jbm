2d6ee1f11639f67b84568dc5d5e42d8e00f1026f
jbm.park <jbm.park@sk.com>
2022-05-03 18:20:28 +0900

    배포-PostsRepository 에 Annotation 추가

--- src/main/java/org/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/org/book/springboot/domain/posts/PostsRepository.java
@@ -1,11 +1,15 @@
 package org.book.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+    @Transactional
+    @Modifying
     @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
     List<Posts> findAllDesc();
 }
